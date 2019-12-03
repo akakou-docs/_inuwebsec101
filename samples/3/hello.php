144fadd3c05d3c3506de7f49efb4d3907f9b0fdc
akakou <akakou571@gmail.com>
2019-12-03 23:29:48 +0900

    GETのサンプルサイトを作成

---
+++ samples/3/hello.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <!-- headで囲まれた部分は見えない -->
+    <meta charset="UTF-8" />
+    <title>Hello</title>
+  </head>
+  <body>
+    <!-- 見出し -->
+    <h1>Hello, <?php echo $_GET['name']?>. </h1>
+    <!-- 文字 -->
+    <p>I'm akakou.</p>
+  </body>
+</html>
